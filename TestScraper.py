03323c44d21e3fda9457836f412d79804d5d1365
audreysoper <74442056+audreysoper@users.noreply.github.com>
2022-10-27 11:04:15 -0400

    trivia added and some lil changes for brevity

--- TestScraper.py
+++ TestScraper.py
@@ -4,15 +4,8 @@ from lib2to3.pgen2 import driver
 from multiprocessing.connection import wait
 import json
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-import time
+
 import csv
 
 
